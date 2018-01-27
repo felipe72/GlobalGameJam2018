2f06f8e862d06c51188f2261e273d82de18ea220
Felipe Osório <felipe.osorio72@outlook.com.br>
2018-01-27 00:22:09 -0200

    Dotween

---
+++ Assets/Scripts/Tile/WallTile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTile : Tile {
+	void Awake(){
+		// onTileEnter;
+	}
+
+	void PushBack(){
+
+	}
+}
