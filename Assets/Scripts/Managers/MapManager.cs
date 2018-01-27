8fd0ecca57ed35209344ef1d53f44a49a29315da
Felipe Osório <felipe.osorio72@outlook.com.br>
2018-01-27 00:03:00 -0200

    isso aí carai

--- Assets/Scripts/Managers/MapManager.cs
+++ Assets/Scripts/Managers/MapManager.cs
@@ -14,6 +14,7 @@ public class MapManager : Singleton<MapManager> {
 
 	[Header("Prefabs")]
 	public GameObject tileGo;
+	public GameObject tileHurtGo;
 	public Player player;
 
 	Tile[,] map;
@@ -27,10 +28,20 @@ public class MapManager : Singleton<MapManager> {
 
 		for (int x = 0; x < tilesWidth; x++) {
 			for (int y = 0; y < tilesHeight; y++) {
-				GameObject go = Instantiate (tileGo, new Vector3 (x, y), Quaternion.identity);
-				go.transform.SetParent (tileTransform);
+				float rng = Random.Range (0f, 1f);
+
+				GameObject go = null;
+
+				if (rng < .2f) {
+					go = Instantiate (tileHurtGo, new Vector3 (x, y), Quaternion.identity);
+					go.transform.SetParent (tileTransform);
+				} else {
+					go = Instantiate (tileGo, new Vector3 (x, y), Quaternion.identity);
+					go.transform.SetParent (tileTransform);
+				}
 
 				Tile tile = go.GetComponent<Tile> ();
+				tile.pos = new Vector3Int (x, y, 0);
 
 				map [x, y] = tile;
 			}
@@ -41,10 +52,18 @@ public class MapManager : Singleton<MapManager> {
 	}
 
 	public Tile GetTileAt(Vector3Int pos){
+		if(pos.x >= tilesWidth || pos.x < 0 || pos.y >= tilesHeight || pos.y < 0){
+			return null;
+		}
+
 		return map [pos.x, pos.y];
 	}
 
 	public bool isValid(Tile tile){
+		if (tile == null) {
+			return false;
+		}
+
 		return true;
 	}
 }
---
+++ Assets/Scripts/Tile/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void TileEvent(MovingUnit unit);
+
+public class Tile : MonoBehaviour{
+	public Vector3Int pos;
+
+	public event TileEvent onTileEnter;
+	public event TileEvent onTileExit;
+
+	public void OnTileEnter(MovingUnit unit){
+		if (onTileEnter != null) {
+			onTileEnter (unit);
+		}
+	}
+
+	public void OnTileExit(MovingUnit unit){
+		if (onTileExit != null) {
+			onTileExit (unit);
+		}
+	}
+}
